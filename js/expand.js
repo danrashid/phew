c8d9bc8b09bffb7332474962ce1a069c7a8848b5
Dan Rashid <drashid@signal.co>
2014-08-29 15:20:33 -0500

    Adding accordion

--- js/expand.js
+++ js/expand.js
@@ -1,23 +1,47 @@
 $(function() {
 
-  $(document).on('click', '.expand', function () {
-    var $a = $(this),
-      $target = $a.next(),
+  function close($expand) {
+    $expand
+      .removeClass('open')
+      .next()
+        .height(null)
+        .css({
+          'padding-top': null,
+          'padding-bottom': null
+        });
+  }
+
+  $(document).on('click', '.expand, .accordion > dt', function () {
+    var $this = $(this),
+      $target = $this.next(),
       targetHeight = $target.height(),
+      targetCss = $target.css(['padding-top', 'padding-bottom']),
       duration;
 
-    $target.height(0);
+    $target
+      .height(0)
+      .css({
+        'padding-top': 0,
+        'padding-bottom': 0
+      });
 
-    if ($a.hasClass('open')) {
+    if ($this.hasClass('open')) {
       duration = parseFloat($target.css('transition-duration')) * 1000;
 
       window.setTimeout(function () {
-        $a.removeClass('open');
-        $target.height(null);
+        close($this);
       }, duration);
     } else {
-      $a.addClass('open');
-      $target.height(targetHeight);
+      $this.addClass('open');
+      close($this.siblings('dt'));
+      $target
+        .height(targetHeight)
+        .css({
+          'padding-top': targetCss['padding-top'],
+          'padding-bottom': targetCss['padding-bottom']
+        });
     }
+
+    return false;
   });
 });
