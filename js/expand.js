adddad6b37d7d7a288f817cafb1957883193c8ac
Dan Rashid <drashid@signal.co>
2014-08-29 13:33:20 -0500

    Using slide effect for expand

--- js/expand.js
+++ js/expand.js
@@ -1,7 +1,23 @@
 $(function() {
 
   $(document).on('click', '.expand', function () {
-    $(this).toggleClass('open');
-    return false;
+    var $a = $(this),
+      $target = $a.next(),
+      targetHeight = $target.height(),
+      duration;
+
+    $target.height(0);
+
+    if ($a.hasClass('open')) {
+      duration = parseFloat($target.css('transition-duration')) * 1000;
+
+      window.setTimeout(function () {
+        $a.removeClass('open');
+        $target.height(null);
+      }, duration);
+    } else {
+      $a.addClass('open');
+      $target.height(targetHeight);
+    }
   });
 });
